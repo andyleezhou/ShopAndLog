505b04dd6b7d8fc86fe65bb36a61071c431c457b
Andy Zhou <andy.lee.zhou@gmail.com>
2019-09-27 14:37:45 -0700

    Updates

---
+++ precache-manifest.bc1faf31fbd31913936a5e4df0032a9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16492856cca0bdfd95b0bc78f475fd72",
+    "url": "/ShopAndLog/index.html"
+  },
+  {
+    "revision": "df30120b0dc1ca6321bd",
+    "url": "/ShopAndLog/static/css/main.e04427b5.chunk.css"
+  },
+  {
+    "revision": "256a6434e5733c13bbf5",
+    "url": "/ShopAndLog/static/js/2.15018151.chunk.js"
+  },
+  {
+    "revision": "df30120b0dc1ca6321bd",
+    "url": "/ShopAndLog/static/js/main.9e6882f9.chunk.js"
+  },
+  {
+    "revision": "5593cb657ce8f18c8e41",
+    "url": "/ShopAndLog/static/js/runtime-main.00378ea4.js"
+  }
+]);
